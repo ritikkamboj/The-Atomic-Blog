84ec1c8ac3f3309a743a6fedb5f2f67282efd090
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-07-23 19:18:03 +0530

    added the useCallBack hook also to avoid the waste rendering

--- src/App-memo.js
+++ src/App-memo.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useMemo, useState } from "react";
+import { memo, useCallback, useEffect, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -25,9 +25,9 @@ function App() {
         )
       : posts;
 
-  function handleAddPost(post) {
+  const handleAddPost = useCallback(function handleAddPost(post) {
     setPosts((posts) => [post, ...posts]);
-  }
+  }, []);
 
   function handleClearPosts() {
     setPosts([]);
@@ -45,13 +45,12 @@ function App() {
   //   show: false,
   //   title: "archive to the main space ",
   // };
-  const object = useMemo(
-    () => ({
+  const object = useMemo(() => {
+    return {
       show: false,
-      title: "archive to main space ",
-    }),
-    []
-  );
+      title: `Post archive to already ${posts.length} main space`,
+    };
+  }, [posts.length]);
   return (
     <section>
       <button
@@ -67,8 +66,8 @@ function App() {
         searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
       />
-      <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive object={object} />
+      <Main posts={searchedPosts} />
+      <Archive object={object} onAddPost={handleAddPost} />
       <Footer />
     </section>
   );
@@ -165,7 +164,7 @@ function List({ posts }) {
   );
 }
 
-const Archive = memo(function Archive({ object }) {
+const Archive = memo(function Archive({ object, onAddPost }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
@@ -177,6 +176,7 @@ const Archive = memo(function Archive({ object }) {
   return (
     <aside>
       <h2>Post archive</h2>
+      <h3>{object.title}</h3>
       <button onClick={() => setShowArchive((s) => !s)}>
         {showArchive ? "Hide archive posts" : "Show archive posts"}
       </button>
@@ -188,7 +188,7 @@ const Archive = memo(function Archive({ object }) {
               <p>
                 <strong>{post.title}:</strong> {post.body}
               </p>
-              {/* <button onClick={() => onAddPost(post)}>Add as new post</button> */}
+              <button onClick={() => onAddPost(post)}>Add as new post</button>
             </li>
           ))}
         </ul>
